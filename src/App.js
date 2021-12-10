9f2750c7a464c2467412e31d5264df88713c1ed7
Matt <matthew@haefelesoftware.com>
2021-12-10 15:53:10 +0200

    implemented color pallete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 function App() {
   return (
-    <div className='flex h-screen text-white'>
-      <div className='bg-gray-800 p-4'>
+    <div className='flex h-screen text-gray-100'>
+      <div className='bg-gray-900 p-3 space-y-2 overflow-y-scroll'>
         <div
           className='bg-white text-gray-800 rounded-full w-12 h-12 flex 
         items-center justify-center'
         >
           TW
         </div>
+        {[...Array(40)].map((_, index) => (
+          <div
+            className='bg-white text-gray-800 rounded-full w-12 h-12 flex 
+        items-center justify-center'
+          >
+            {index}
+          </div>
+        ))}
       </div>
-      <div className='bg-gray-700 flex flex-col w-60'>
-        <div className='p-4 shadow-md'>Tailwind CSS</div>
-        <div className='p-4 flex-1'>List</div>
+      <div className='bg-gray-800 flex flex-col w-60'>
+        <div className='px-3 h-12 flex items-center shadow-md'>
+          Tailwind CSS
+        </div>
+        <div className='text-gray-300 p-3 flex-1 space-y-2 overflow-y-scroll'>
+          <p className='text-white'>channel (unread)</p>
+          {[...Array(40)].map((_, index) => (
+            <p>channel {index}</p>
+          ))}
+        </div>
       </div>
-      <div className='bg-gray-600 flex-1'>
-        <div className='p-4 shadow-md'>General</div>
-        <div className='p-4 flex-1'>Messages</div>
+      <div className='bg-gray-700 flex flex-1 flex-col'>
+        <div className='px-3 h-12 flex items-center shadow-md'>General</div>
+        <div className='p-3 flex-1 space-y-4 overflow-y-scroll'>
+          {[...Array(40)].map((_, index) => (
+            <p>
+              Message {index} :Bacon ipsum dolor amet kielbasa ground round
+              bacon, cow jowl andouille filet mignon pork belly tri-tip doner
+              burgdoggen shank turkey. Meatball doner burgdoggen pastrami tail
+              shank andouille ham brisket alcatra pork loin prosciutto
+              tenderloin meatloaf boudin. Drumstick venison filet mignon boudin
+              beef biltong.
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: '#ECEDEE',
+          100: '#DCDDDE',
+          200: '#B9BBBE',
+          300: '#8E9297',
+          400: '#72767D',
+          500: '#5C6067',
+          600: '#464950',
+          700: '#36393F',
+          800: '#2F3136',
+          900: '#202225',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
